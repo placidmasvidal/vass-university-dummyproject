1b2f83b1ab41c058b8a442759999e1f1987346bc
Placid Masvidal <placid.masvidal@vasscompany.com>
2026-03-26 11:21:38 +0100

    tests for lab 1.1 added

---
+++ core/src/test/java/com/vasscompany/dummyproject/core/services/LegacyPriceCalculatorDebugMain.java
@@ -0,0 +1,20 @@
+package com.vasscompany.dummyproject.core.services.lab1_1RefactoringCopilot.impl;
+
+public class LegacyPriceCalculatorDebugMain {
+
+    public static void main(String[] args) {
+        LegacyPriceCalculatorServiceImpl svc = new LegacyPriceCalculatorServiceImpl();
+
+        System.out.println("calc(10, 5, 20, 4)   = " + svc.calc(10, 5, 20, 4));
+        System.out.println("calc(2, 3, 10, 2)    = " + svc.calc(2, 3, 10, 2));
+        System.out.println("calc(10, 5, 20, 0)   = " + svc.calc(10, 5, 20, 0));
+        System.out.println("calc(-10, 5, 20, 4)  = " + svc.calc(-10, 5, 20, 4));
+
+        System.out.println("proc(\"A\", 500, null) = " + svc.proc("A", 500, null));
+        System.out.println("proc(\"C\", -1, \"cfg\") = " + svc.proc("C", -1, "cfg"));
+
+        // Opcional: probar getI()
+        System.setProperty("demo.key", "valor-demo");
+        System.out.println("getI(\"demo\", \"key\") = " + svc.getI("demo", "key"));
+    }
+}
